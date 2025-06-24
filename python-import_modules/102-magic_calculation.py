5c0b15c13c81ff19782843c8d4e5978f6a53f54a
EucTech <euccreative@gmail.com>
2025-06-24 13:21:02 -0700

    102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
+    """Python bytecode"""
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for j in range(4, 7):
-            c = add(c, j)
-        return c
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
     else:
-        return sub(a, b)
+        return (sub(a, b))
