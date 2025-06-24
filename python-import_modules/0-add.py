2e503bbe6d0a0efd4dfbce485e64a7fa194af08f
EucTech <euccreative@gmail.com>
2025-06-24 12:37:58 -0700

    updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,5 +3,5 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    sum_num = add(a, b)
-    print(f"{a} + {b} = {sum_num}")
+    value = add(a, b)
+    print("{} + {} = {}".format(a, b, value))
