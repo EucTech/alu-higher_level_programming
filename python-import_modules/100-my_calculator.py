7ebc0e4a5be55a2956a4c592727b26d7c747ace4
EucTech <euccreative@gmail.com>
2025-06-24 13:10:24 -0700

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
     length = len(sys.argv) - 1
     if length != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        operators = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in operators.keys():
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        x = int(sys.argv[1])
-        y = int(sys.argv[3])
-        z = sys.argv[2]
-        print("{} {} {} = {}".format(x, z, y, operators[z](x, y)))i
+        print(" Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    sign = sys.argv[2]
+    if sign != '+' and sign != '-' and sign != '*' and sign != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sign == "+":
+        value = add(a, b)
+    if sign == "-":
+        value = sub(a, b)
+    if sign == "*":
+        value = mul(a, b)
+    if sign == "/":
+        value = div(a, b)
+
+    print(f"{a} {sign} {b} = {value}")
