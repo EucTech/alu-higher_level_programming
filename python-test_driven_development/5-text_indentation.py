d1b8feb343a2448d09793e867e41bcaffe1653d1
EucTech <euccreative@gmail.com>
2025-08-01 10:08:37 -0700

    tasks

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A function that print a text with 2 new lines after '.','?',':'"""
+
+
+def text_indentation(text):
+    """This is the function that prints text_indentation"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    if '.' in text:
+        text = text.replace('.', '.\n\n')
+    if '?' in text:
+        text = text.replace('?', '?\n\n')
+    if ':' in text:
+        text = text.replace(':', ':\n\n')
+    text = "\n".join(line.lstrip() for line in text.strip().split("\n"))
+    print(text)
