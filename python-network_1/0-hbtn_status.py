d0ba15bee751534749cd7c0040daa6d36d1b33c3
EucTech <euccreative@gmail.com>
2025-08-01 11:04:04 -0700

    fixed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""A script that fetches https://alx-intranet.hbtn.io/status"""
+"""A script that prints body"""
+
 
 import urllib.request
 
 
-if __name__ == "__main__":
-    with urllib.request.\
-            urlopen('https://alx-intranet.hbtn.io/status') as response:
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
-        content_type = type(content)
-        utf8_content = content.decode('utf-8')
-
-    print("Body response:")
-    print(f"\t- type: {content_type}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {utf8_content}")
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
